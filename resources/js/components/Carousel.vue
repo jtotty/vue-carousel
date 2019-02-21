bebf4c3e274051a52102ecfdb3f4debcfaf37fb7
James Totty <james.totty@speechlink.co.uk>
2019-02-21 17:24:37 +0000

    It's alive!

---
+++ resources/js/components/Carousel.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    import Flickity from 'flickity';
+    import imagesLoaded from 'flickity-imagesloaded';
+    import 'flickity/dist/flickity.min.css';
+
+    export default {
+        props: {
+            wraparound: { default: true },
+            autoplay: { default: false }
+        },
+
+        mounted() {
+            new Flickity(this.$el, {
+                wrapAround: this.wraparound,
+                autoPlay: this.autoplay,
+                cellAlign: 'left',
+                contain: true,
+                imagesLoaded: true
+            });
+        }
+    }
+</script>
